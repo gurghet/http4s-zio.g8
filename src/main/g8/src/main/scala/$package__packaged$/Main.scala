a535c186d06afe120389bcf511117f6a8315ddfc
Andrea Passaglia <gurghet@gmail.com>
2019-06-15 12:27:05 +0100

    add zio as F

--- src/main/g8/src/main/scala/$package__packaged$/Main.scala
+++ src/main/g8/src/main/scala/$package__packaged$/Main.scala
@@ -1,10 +1,15 @@
 package $package$
 
-import cats.effect.{ExitCode, IO, IOApp}
 import cats.implicits._
 import scalaz.zio.interop.catz._
+import scalaz.zio.interop.catz.implicits._
+import scalaz.zio.App
+import scalaz.zio.ZIO
+import scalaz.zio.Task
 
-object Main extends IOApp {
-  def run(args: List[String]) =
-    $name;format="Camel"$Server.stream[IO].compile.drain.as(ExitCode.Success)
+object Main extends App {
+  override def run(args: List[String]): ZIO[Environment,Nothing,Int] =
+    ZIO.runtime[Environment].flatMap { implicit rt =>
+      $name;format="Camel"$Server.stream[Task].compile.drain.fold(_ => 1, _ => 0)
+    }
 }
