fa6b5cb84a7a6595cd9cbb4e8c4a1924c44e401c
Andrea Passaglia <gurghet@gmail.com>
2019-07-20 16:28:12 +0100

    fix package

--- src/main/g8/src/main/scala/$package__packaged$/configuration/package.scala
+++ src/main/g8/src/main/scala/$package__packaged$/configuration/package.scala
@@ -1,6 +1,6 @@
 package $package$
 
-import scalaz.zio.TaskR
+import zio.TaskR
 
 package object configuration extends Configuration.Service[Configuration] {
   override def load: TaskR[Configuration, ServerConfig] = TaskR.accessM(_.configuration.load)
